da300c001db6faa6bc04cfb56641599259b70bf1
Ewelina Kuzmicka <ekuzmicka@pk.edu.pl>
2019-05-06 22:20:32 +0200

    Functionality of Chronometer
    Counting time of record and saving it into variable
    Start/stop recording event

--- app/src/main/java/com/dawidrazny/audiohandler/MainActivity.java
+++ app/src/main/java/com/dawidrazny/audiohandler/MainActivity.java
@@ -22,46 +22,65 @@ public class MainActivity extends AppCompatActivity {
     int ChronometerTickCounter = 0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
 
         // home screen layout elements binding
-        mRecordButton = (Button)findViewById(R.id.recordButton);
-        mNotificationTextView = (TextView)findViewById(R.id.notificationTextView);
+        mRecordButton = (Button) findViewById(R.id.recordButton);
+        mNotificationTextView = (TextView) findViewById(R.id.notificationTextView);
         mChronometer = (Chronometer) findViewById(R.id.chronometer);
 
         // Record Button events ( start & record, pause and stop )
-        mRecordButton.setOnClickListener(new View.OnClickListener()
-        {
+        mRecordButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
-                Toast.makeText( v.getContext() , "Recording started!", Toast.LENGTH_LONG).show();
-
-                // Chronometer start counting time
-                mChronometer.setBase(SystemClock.elapsedRealtime());
-                mChronometer.start();
-                mChronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener()
-                {
-                    @Override
-                    public void onChronometerTick(Chronometer chronometer) {
-                        if (ChronometerTickCounter == 0)
-                        {
-                            mNotificationTextView.setText("Recording.");
-                        } else if (ChronometerTickCounter == 1)
-                        {
-                            mNotificationTextView.setText("Recording..");
-                        } else if (ChronometerTickCounter == 2)
-                        {
-                            mNotificationTextView.setText("Recording...");
-                            ChronometerTickCounter = -1;
-                        }
-                        ChronometerTickCounter++;
-                    }
-                });
+            public void onClick(View v) {
+                onRecord(isRecording);
             }
         });
+    }
+
+    private void onRecord(boolean event)
+    {
+
+        //record
+        if(!event)
+        {
+            Toast.makeText(this, "Recording started!", Toast.LENGTH_LONG).show();
+
+            // Chronometer start counting time
+            mChronometer.setBase(SystemClock.elapsedRealtime());
+            mChronometer.start();
+            // changing a text in notification textview
+            mChronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener()
+            {
+                @Override
+                public void onChronometerTick(Chronometer chronometer) {
+                    if (ChronometerTickCounter == 0) {
+                        mNotificationTextView.setText("Recording.");
+                    } else if (ChronometerTickCounter == 1) {
+                        mNotificationTextView.setText("Recording..");
+                    } else if (ChronometerTickCounter == 2) {
+                        mNotificationTextView.setText("Recording...");
+                        ChronometerTickCounter = -1;
+                    }
+                    ChronometerTickCounter++;
+                }
+            });
+
+            //trigering recording event
+            isRecording = true;
+        }
+        // stop recording
+        else if(event)
+        {
+            Toast.makeText(this, "Recording stoped!" + mChronometer.getText().toString(), Toast.LENGTH_LONG).show();
+
+            mChronometer.stop();
+            mChronometer.setBase(SystemClock.elapsedRealtime());
+
+            //trigering recording event
+            isRecording = false;
+        }
     }
 }
